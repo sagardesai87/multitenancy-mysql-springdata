7664f52af7957c96d02ecd7e576eed4212735339
sagardesai <sagar.desai@irisbusiness.com>
2024-04-21 23:21:57 +0530

    initial commit

---
+++ multitenancy.separateschema/src/test/java/demo/multitenancy/separateschema/ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.multitenancy.separateschema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
